24889254ff7b018cde2e8fe67716efb7966b533b
Scorpion <806443649@qq.com>
2020-04-01 06:54:28 -0400

    update day02

--- src/part/sql2.py
+++ src/part/sql2.py
@@ -15,9 +15,19 @@ sql = '''
       SELECT a.insert_id, a.device_num, a.GNSS_time , b.dist_province , b.dist_city , b.dist_district,b.dist_township FROM device_position_act b INNER JOIN device_position_infomation a ON a.insert_id = b.insert_id;
       '''
 
-# read_sql_query的两个参数: sql语句， 数据库连接
-df2 = pd.read_sql_query(sql, engine)
+sql2 = '''
+      SELECT dpcid,province,city,district,township,count,date FROM `beidou_dev`.`daily_place_count`  WHERE `date` = '2019-10-28' 
+      '''
 
-df2.to_csv('data2.csv',index=False)
-df2.to_pickle('D.pkl')
+sql3 = '''
+      SELECT dpacid,start_province,start_city,start_district,start_township,end_province,end_city,end_district,end_township,count,date FROM `beidou_dev`.`daily_path_count`  WHERE `date` = '2019-10-28' 
+      '''
+# read_sql_query的两个参数: sql语句， 数据库连接
+df2 = pd.read_sql_query(sql2, engine)
+df2.to_csv('data_dpc.csv',index=False)
+df2.to_pickle('D_dpc.pkl')
+
+df3 = pd.read_sql_query(sql3, engine)
+df3.to_csv('data_dpac.csv',index=False)
+df3.to_pickle('D_dpac.pkl')
 
